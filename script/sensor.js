c5798652f6e763317909ebf9dc08a78efe8527e0
niggi98 <bacher.nico@gmail.com>
2022-04-28 12:58:06 +0200

    changed buttons on sensor page

--- script/sensor.js
+++ script/sensor.js
@@ -12,21 +12,24 @@ function Start()
     ForceUpdate("Web2Plc.tankLevel1mm");    // immediate initialization of the value visualization
     ForceUpdate2("Web2Plc.level1mA");
     changeTank("Web2Plc.tankLevel1mm");
+    updateInput(NaN);
     setTimeout("OnTimer()",100);
     setTimeout("OnTimer2()",150);
+    setTimeout("OnTimer3()",200)
 
 
     elementStart = document.getElementById('start1');
-    elementStart.addEventListener("click", () => {
-        document.getElementById("pump1running").setAttribute("fill", "green");
-        document.getElementById("pump1stopped").setAttribute("fill", "white");
-    });
+    // elementStart.addEventListener("click", () => {
+
+    //     document.getElementById("pump1running").setAttribute("fill", "green");
+    //     document.getElementById("pump1stopped").setAttribute("fill", "white");
+    // });
     
     elementStart = document.getElementById('stop1');
-    elementStart.addEventListener("click", () => {
-        document.getElementById("pump1running").setAttribute("fill", "white");
-        document.getElementById("pump1stopped").setAttribute("fill", "red");
-    });
+    // elementStart.addEventListener("click", () => {
+    //     document.getElementById("pump1running").setAttribute("fill", "white");
+    //     document.getElementById("pump1stopped").setAttribute("fill", "red");
+    // });
 }
 
 // The page update11.dat solely consists of a reference to the variable "Dynvalue".
@@ -58,6 +61,18 @@ function OnTimer2()
         // this asynchronous method does silently update the data within the browser
     }
     setTimeout("OnTimer2()", 250);  // the function OnTimer is to be called every 200 ms
+}
+function OnTimer3() 
+{
+    if (! g_bPageRequested) 
+    {
+        g_bPageRequested = true;
+        // request the update page asynchronously, function UpdateCallback is called if response
+        // has been received           
+        DoHttpRequest(this, "update.dat", UpdateCallback3, true);
+        // this asynchronous method does silently update the data within the browser
+    }
+    setTimeout("OnTimer3()", 100);  // the function OnTimer is to be called every 200 ms
 }
 
 // update11.dat has been received
@@ -106,14 +121,44 @@ function OnTimer2()
         else {break;}		
     }
     dynValue3 = results[10].substr(count, signs.length);
+
     var dynValueInt3 = parseFloat(dynValue3);
+
+    var signs = results[7].split("");
+    var i;
+    var count = 0;
+    for (i = 0; i < signs.length; i++) {
+        //Check if the first signs are numbers
+        if (true == isNaN(signs[i])) {
+            count = count + 1;
+        }
+        else {break;}		
+    }
+    dynValue4 = results[7].substr(count, signs.length);
+
+    var dynValueInt4 = parseFloat(dynValue4);
+
+    var signs = results[8].split("");
+    var i;
+    var count = 0;
+    for (i = 0; i < signs.length; i++) {
+        //Check if the first signs are numbers
+        if (true == isNaN(signs[i])) {
+            count = count + 1;
+        }
+        else {break;}		
+    }
+    dynValue5 = results[8].substr(count, signs.length);
+    var dynValueInt5 = parseFloat(dynValue5);
 
 
     if (status < 300) {// check HTTP response status
         ForceUpdate(dynValueInt);         // update with the provided value  
         changeTank(dynValueInt);
         changeColorTankEmpty(dynValueInt2);
-        changeColorResevoir(dynValueInt3)
+        changeColorResevoir(dynValueInt3);
+        changeColorRunning(dynValueInt4);
+        changeColorStopped(dynValueInt5);
 
         g_bPageRequested = false;
         setTimeout("OnTimer()", 200);  // the function OnTimer is to be called in 200 ms
@@ -168,6 +213,45 @@ function UpdateCallback2(obj, response, status) {
         setTimeout("OnTimer2()", 1100);  // the function OnTimer is to be called in 1 sec
     }
 }
+function UpdateCallback3(obj, response, status) {
+    var ok;
+    //Splitting the results
+    var results = response.split(" ");
+    //Splitting the results in single signs
+    var signs = results[3].split("");
+    var i;
+    var count = 0;
+    for (i = 0; i < signs.length; i++) {
+        //Check if the first signs are numbers
+        if (true == isNaN(signs[i])) {
+            count = count + 1;
+        }
+        else {break;}		
+    }
+    //delete signs which aren't numbers
+    dynValue = results[3].substr(count, signs.length);
+    
+    var dynValueInt = parseInt(dynValue);
+
+    if (status < 300) {// check HTTP response status 
+        updateInput(dynValueInt); 
+       
+        g_bPageRequested = false;
+        setTimeout("OnTimer3()", 100);  // the function OnTimer is to be called in 200 ms
+        return;
+    }
+    if (status == 503) {               // service currently unvailable , server overloaded 
+        ok = confirm(dynValueInt);
+    } else {
+        ok = confirm("FAILED: HTTP error " + status);
+    }
+    g_bPageRequested = false;
+    if (ok) {
+        setTimeout("OnTimer3()", 800);  // the function OnTimer is to be called in 1 sec
+    }
+}
+
+
 // Within the page update11_ajax.html or update11_ajax.js the function ForceUpdate is called with the current value.
 // This value (0..255) controls the width of table "table2" within the table "table1"
 function ForceUpdate(val) 
@@ -218,10 +302,32 @@ function changeColorResevoir(newValue)
 			
 }
 
+function changeColorRunning(newValue)
+{
+	if (newValue==1) {
+        document.getElementById("pump1running").setAttribute("fill", "green");
+      }
+    if (newValue==0) {
+        document.getElementById("pump1running").setAttribute("fill", "white");
+    } 
+			
+}
+
+function changeColorStopped(newValue)
+{
+	if (newValue==1) {
+        document.getElementById("pump1stopped").setAttribute("fill", "red");
+      }
+    if (newValue==0) {
+        document.getElementById("pump1stopped").setAttribute("fill", "white");
+    } 
+			
+}
+
 function changeColorTankEmpty(newValue)
 {
 	if (newValue==1) {
-        document.getElementById("tank1empty").setAttribute("fill", "red");
+        document.getElementById("tank1empty").setAttribute("fill", "yellow");
       }
     if (newValue==0) {
         document.getElementById("tank1empty").setAttribute("fill", "white");
@@ -229,5 +335,19 @@ function changeColorTankEmpty(newValue)
 			
 }
 
+function updateInput(indicator){
+ 
+    if(indicator > 1){
+        document.getElementById("exper1").style.background = "#ffcccb";
+        document.getElementById("exper1").value = "Deactivate";   
+        document.getElementById("value1").value = 0;
+
+    }
+    else{
+        document.getElementById("exper1").style.background = "#ddeedc";
+        document.getElementById("exper1").value = "Activate";  
+        document.getElementById("value1").value = 1;  
+    }
+}
 
 //buttons für operator ansteuern, platzierung der einzelnen felder, buttons von Jens, Tankanzeige
